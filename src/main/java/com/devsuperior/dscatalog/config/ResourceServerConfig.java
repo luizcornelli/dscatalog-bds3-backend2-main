b865dd7af94ed3ae2388e2a998b121090cd6b04c
Luiz Cornelli <luizcornelli.dev@gmail.com>
2022-05-26 20:08:53 -0300

    H2 security configuration

--- src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,11 +15,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
+	
+	@Autowired
+	private Environment environment; // Objeto do ambiente de execução da aplicação 
+	
 	@Autowired
 	private JwtTokenStore jwtTokenStore; 
 	
-	private static final String[] PUBLIC = {"/oauth/token"};
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 	private static final String[] OPERADOR_OR_ADMIN = {"/products/**", "/categories/**"};
 	private static final String[] ADMIN = {"/users/**"};
 
@@ -28,6 +34,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override// Configurando as rotas
 	public void configure(HttpSecurity http) throws Exception {
+		
+		// H2 liberando frames
+		if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
