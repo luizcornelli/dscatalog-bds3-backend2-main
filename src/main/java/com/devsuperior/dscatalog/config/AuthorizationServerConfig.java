2af0bd9664ba0f545ade3309c31b5df9e555fade
Luiz Cornelli <luizcornelli.dev@gmail.com>
2022-05-26 11:24:07 -0300

    Informações adicionais no token

--- src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +12,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.devsuperior.dscatalog.components.JwtTokenEnhancer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -38,6 +43,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private JwtTokenEnhancer jwtTokenEnhancer; 
+	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permittAll()").checkTokenAccess("isAuthenticated()");
@@ -55,9 +63,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override // Quem vai autorizar e qual vai ser o formato do token
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtAccessTokenConverter, jwtTokenEnhancer));
+		
 		endpoints.authenticationManager(authenticationManager)
 		.tokenStore(jwtTokenStore)
-		.accessTokenConverter(jwtAccessTokenConverter);
+		.accessTokenConverter(jwtAccessTokenConverter)
+		.tokenEnhancer(tokenEnhancerChain);
 	}
 	
 	
